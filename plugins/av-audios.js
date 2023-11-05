762b94b990cf8e24c623220f6ba148049b1f6032
C7836 M0N5T3R L4LLU <87799191+Monsterlallu@users.noreply.github.com>
2023-11-05 14:51:02 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -7,6 +7,10 @@ handler.all = async function (m) {
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
+  if (!!message.mention && message.mention[0] == '917736129238@s.whatsapp.net') {
+  await message.client.sendMessage(message.jid, fs.readFileSync('./src/mp3/itstaurus.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
+ }
+    
   if (/^bgm$/i.test(m.text) ) {
     let av = './src/mp3/bgm.mp3'
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
@@ -16,6 +20,16 @@ handler.all = async function (m) {
     let av = './src/mp3/love.mp3'
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
+
+  if (/^lallu$/i.test(m.text) ) {
+    let av = './src/mp3/Lallu.mp3'
+    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+  }
+
+  if (/^welcome$/i.test(m.text) ) {
+    let av = './src/mp3/welcome.mp3'
+    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+  }
 
   if (/^.alive$/i.test(m.text) ) {
     let av = './src/mp3/.alive.mp3'
